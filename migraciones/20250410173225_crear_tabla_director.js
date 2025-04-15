c962d988af4682409822dcf7a203974b0bcbda66
Anett <anettmava@gmail.com>
2025-04-14 19:22:28 -0600

    creación de tablas para registro de escuela, gitignore nodemodules, escuela.js y auth.js cambios

---
+++ migraciones/20250410173225_crear_tabla_director.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+    return knex.schema.createTable("Director", (table) => {
+      table.string("CCT", 20).primary().references("CCT").inTable("Escuela").onDelete("CASCADE");
+      table.date("fechaJubilacion");
+      table.boolean("posibleCambioPlantel").defaultTo(false);
+      table.string("nombre").notNullable();
+      table.string("correoElectronico").notNullable();
+      table.string("telefono").notNullable();
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("Director");
+  };
---
+++ migraciones/20250410173251_crear_tabla_mesa_supervisor.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+    return knex.schema.createTable("Supervisor", (table) => {
+      table.string("CCT", 20).primary().references("CCT").inTable("Escuela").onDelete("CASCADE");
+      table.date("fechaJubilacion");
+      table.boolean("posibleCambioZona").defaultTo(false);
+      table.string("medioContacto").notNullable();
+      table.integer("antiguedadZona").notNullable();
+      table.string("nombre").notNullable();
+      table.string("correoElectronico").notNullable();
+      table.string("telefono").notNullable();
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("Supervisor");
+  };
