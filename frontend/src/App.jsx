af45850b56bc77a72110591ba17c2e90f71a9a1e
Marijo <mjmc12.22@gmail.com>
2025-04-19 16:31:20 -0600

    ya quedo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import RegisterSchool from './pages/RegisterSchool';
+import PageAdmin from './pages/PageAdmin';
 
 function App() {
-  return <RegisterSchool />;
+  return <PageAdmin/>;
 }
 
 export default App;
