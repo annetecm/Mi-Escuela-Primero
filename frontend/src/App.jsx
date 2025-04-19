3a1d953b3e957c86021417701ee131b16fd6fb66
Marijo <mjmc12.22@gmail.com>
2025-04-19 17:01:09 -0600

    correciones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginPage from './pages/LoginPage';
 import RegisterSchool from './pages/RegisterSchool';
-import PageAdmin from './pages/PageAdmin';
+import AllyRegSuccess from './pages/AllyRegSuccess';
 
 function App() {
-  return <PageAdmin/>;
+  const [currentPage, setCurrentPage] = useState('login');
+
+  const handleRedirectToRegister = () => {
+    setCurrentPage('register-school');
+  };
+
+  const handleRegistrationSuccess = () => {
+    setCurrentPage('registration-success');
+  }
+
+
+
+  return (
+    <>
+      {currentPage === 'login' && (
+        <LoginPage onRegisterSchool={handleRedirectToRegister} />
+      )}
+      {currentPage === 'register-school' && (
+        <RegisterSchool onRegistrationSuccess={handleRegistrationSuccess} />
+      )}
+      {currentPage === 'registration-success' && <AllyRegSuccess />}
+    </>
+  );
 }
 
 export default App;
