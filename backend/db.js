f02e7587e358765d036d0ab707b4ac3eacc777af
Mariano <a01639413@tec.mx>
2025-04-20 20:08:24 -0600

    login hechos

--- backend/db.js
+++ backend/db.js
@@ -1,22 +1,19 @@
 const { Pool } = require('pg');
-require('dotenv').config({ path: '../.env' }); // Ajusta la ruta según tu estructura
-
-// Configuración condicional para SSL
-const sslConfig = process.env.NODE_ENV === 'production' ? { 
-  rejectUnauthorized: false 
-} : false;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: sslConfig
+  user: 'fakeadmin',
+  host: 'localhost',
+  database: 'testdb',
+  password: 'fakeadmin', // pon la contraseña correcta
+  port: 5432,
+  ssl: false
 });
 
-// Verificación de conexión mejorada
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error(' Error al conectar:', err.message);
+    return console.error('❌ Error al conectar:', err.message);
   }
-  console.log(' Conexión a DB establecida');
+  console.log('✅ Conexión a DB establecida');
   release();
 });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,13 @@ import LoginPage from './pages/LoginPage';
 import RegisterSchool from './pages/RegisterSchool';
 import AllyRegSuccess from './pages/AllyRegSuccess';
 import RegisterAlly from './pages/RegisterAlly'; 
+import SchoolProfile from './pages/SchoolProfile';
+import AllyProfile from './pages/AllyProfile';
+import EditSchool from './pages/EditSchool';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('login');
+  const [userType, setUserType] = useState(null); // ❌
 
   const handleRedirectToRegisterSchool = () => {
     setCurrentPage('register-school');
@@ -24,9 +28,13 @@ function App() {
     <>
       {currentPage === 'login' && (
         <LoginPage 
-          onRegisterSchool={handleRedirectToRegisterSchool} 
-          onRegisterAlly={handleRedirectToRegisterAlly} // 👈 pásalo como prop
-        />
+        onRegisterSchool={handleRedirectToRegisterSchool} 
+        onRegisterAlly={handleRedirectToRegisterAlly}
+        onLoginSuccess={(tipo) => {
+        setUserType(tipo);      
+        setCurrentPage(tipo === 'escuela' ? 'school-profile' : 'ally-profile'); 
+        }}
+      />
       )}
       {currentPage === 'register-school' && (
         <RegisterSchool onRegistrationSuccess={handleRegistrationSuccess} />
@@ -35,6 +43,12 @@ function App() {
         <RegisterAlly onRegistrationSuccess={handleRegistrationSuccess}/>
       )}
       {currentPage === 'registration-success' && <AllyRegSuccess />}
+      {currentPage === 'school-profile' && (
+          <SchoolProfile onEditClick={() => setCurrentPage('edit-school')} />
+        )}
+      {currentPage === 'edit-school' && <EditSchool />}
+      {currentPage === 'ally-profile' && <AllyProfile />}
+
     </>
   );
 }
